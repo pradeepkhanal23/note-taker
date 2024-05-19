5baba88f4eb07ecb24251be1c1eee82899b7553e
Pradeep Khanal <pradeepkhanal642@gmail.com>
2024-05-19 20:33:24 +1000

    delete method added

--- routes/notes.js
+++ routes/notes.js
@@ -2,8 +2,8 @@
 const notes = require("express").Router();
 const {
   readFromFile,
-  writeToFile,
   readAndAppend,
+  writeToFile,
 } = require("../helpers/fsUtils");
 
 const uuid = require("../helpers/uuid");
@@ -13,8 +13,9 @@ notes.get("/", (req, res) => {
   console.info(`${req.method} request received`);
 
   readFromFile("./db/notes.json").then((data) => {
+    let parsedData = JSON.parse(data);
     // because the readFile method returns a stringified data, we need to convert it to js object using JSON.parse()  method before sending it to the user
-    return res.status(200).json(JSON.parse(data));
+    return res.status(200).json(parsedData);
   });
 });
 
@@ -40,6 +41,40 @@ notes.post("/", (req, res) => {
       error: "POST request failed",
     });
   }
+});
+
+//DELETE method for the notes
+notes.delete("/:noteId", (req, res) => {
+  console.log(`${req.method} method received`);
+
+  readFromFile("./db/notes.json")
+    .then((data) => {
+      const receivedId = req.params.noteId;
+
+      // because the readFile method returns a stringified data, we need to convert it to js object using JSON.parse()  method before sending it to the user
+      let savedNotes = JSON.parse(data);
+
+      //filtering the notes based on the id
+      savedNotes = savedNotes.filter((note) => {
+        if (note.id !== receivedId) {
+          return note;
+        }
+      });
+
+      //writing the new updated notes to the db/notes.json file
+      writeToFile("./db/notes.json", savedNotes);
+
+      //successful status code send back to the user
+      res.status(200).send({
+        message: "note successfully deleted",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send({
+        message: "couldn't read the file",
+      });
+    });
 });
 
 //exporting notes router
